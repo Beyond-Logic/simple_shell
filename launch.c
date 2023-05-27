aaa41244462f378bda91de5bbbb79e0cbe75540a
Beyond-Logic <edemaero@gmail.com>
2023-05-27 00:30:13 +0000

    Simple Shell Updates

--- launch.c
+++ launch.c
@@ -8,29 +8,31 @@
 
 int launch(char **args)
 {
-	int i;
-
-	char *builtins[] = {
-		"cd",
-		"exit",
-		NULL
-	};
-
 	if (args[0] == NULL)
 	{
 		return (1);
 	}
 
-	for (i = 0; builtins[i]; i++)
+	if (strcmp(args[0], "cd") == 0)
 	{
-		if (strcmp(args[0], builtins[i]) == 0)
+		if (args[1] == NULL)
 		{
-			if (strcmp(args[0], "exit") == 0)
-			{
-				exit(0);
-			}
-			return (1);
+			fprintf(stderr, "cd: missing argument\n");
 		}
+		else
+		{
+			if (chdir(args[1]) != 0)
+			{
+				perror(args[0]);
+			}
+		}
+
+		return (1);
+	}
+
+	else if (strcmp(args[0], "exit") == 0)
+	{
+		exit(0);
 	}
 
 	return (execute(args));
